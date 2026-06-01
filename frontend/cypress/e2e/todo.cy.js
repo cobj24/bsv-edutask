50615a9e0644da63788644e5ddfeb57070eb8493
cob <cobj24@student.bth.se>
2026-06-01 13:36:11 +0200

    Add after block to clean up database after tests

--- frontend/cypress/e2e/todo.cy.js
+++ frontend/cypress/e2e/todo.cy.js
@@ -1,4 +1,17 @@
 describe('R8 - Todo list manipulation', () => {
+
+  after(() => {
+    // Hämta användarens ID via email
+    cy.request('GET', 'http://localhost:5000/users/bymail/jane.doe@gmail.com')
+      .then(response => {
+        const userId = response.body._id.$oid
+        // Radera användaren (och alla dess tasks/todos)
+        cy.request('DELETE', `http://localhost:5000/users/${userId}`)
+      })
+    // Återskapa testdata
+    cy.request('POST', 'http://localhost:5000/populate')
+  })
+
   beforeEach(() => {
     cy.visit('http://localhost:3000')
     cy.get('input[type=text]').first().type('jane.doe@gmail.com')
@@ -6,7 +19,7 @@ describe('R8 - Todo list manipulation', () => {
     cy.get('.container-element a').first().click()
   })
 
-  // R8UC1 - TC1: add a todo when there is a discription
+  // R8UC1 - TC1: add a todo when there is a description
   it('TC1: add a new todo-item', () => {
     cy.get('input[placeholder="Add a new todo item"]').type('my new todo', {force: true})
     cy.get('input[value="Add"]').click({force: true})
@@ -22,18 +35,18 @@ describe('R8 - Todo list manipulation', () => {
   it('TC3: toggle an active todo -> done', () => {
     cy.get('.checker').first().click()
     cy.get('.checker').first().should('have.class', 'checked')
-    })
+  })
 
-    // R8UC2 - TC4: Toggle a done todo-item back to active
+  // R8UC2 - TC4: Toggle a done todo-item back to active
   it('TC4: toggle a done todo-item back to active', () => {
-    cy.get('.checker').first().click() // done
-    cy.get('.checker').first().click() // toggla back to active
+    cy.get('.checker').first().click()
+    cy.get('.checker').first().click()
     cy.get('.checker').first().should('have.class', 'unchecked')
-    })
+  })
 
+  // R8UC3 - TC5: Remove a todo-item
   it('TC5: removes a todo-item', () => {
     cy.get('.remover').first().click()
     cy.get('.todo-list').should('not.contain.text', 'Watch video')
-    })
+  })
 })
-
